236a52b8f01cbdc1107d05e018ca9b7e00484df2
preecej <preecej@science.oregonstate.edu>
2017-10-17 14:46:55 -0700

    Disabled Stable ID link in Description tb on browser detail panel

--- src/main/java/org/reactome/web/pwp/client/details/common/widgets/panels/StableIdentifierPanel.java
+++ src/main/java/org/reactome/web/pwp/client/details/common/widgets/panels/StableIdentifierPanel.java
@@ -1,6 +1,10 @@
 package org.reactome.web.pwp.client.details.common.widgets.panels;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import org.reactome.web.pwp.model.classes.DatabaseObject;
 import org.reactome.web.pwp.model.classes.StableIdentifier;
 
@@ -22,10 +26,22 @@ public class StableIdentifierPanel extends DetailsPanel implements TransparentPa
 
     private void initialize(){
         String identifier = stableIdentifier.getDisplayName();
-        Anchor link = new Anchor(identifier, "/cgi-bin/control_panel_st_id?ST_ID=" + identifier);
-        link.setTarget("_blank");
-        link.setTitle("Go to REACTOME control panel for " + identifier);
-        initWidget(link);
+
+        HorizontalPanel hp = new HorizontalPanel();
+        hp.getElement().getStyle().setMarginBottom(10, Style.Unit.PX);
+
+        Label title = new Label(identifier);
+        Style titleStyle = title.getElement().getStyle();
+        titleStyle.setFontWeight(Style.FontWeight.NORMAL);
+        titleStyle.setMarginRight(5, Style.Unit.PX);
+        //hp.add(title);
+        //hp.add(new HTMLPanel(sb.toString()));
+
+        //Anchor link = new Anchor(identifier, "/cgi-bin/control_panel_st_id?ST_ID=" + identifier);
+        //link.setTarget("_blank");
+        //link.setTitle("Go to REACTOME control panel for " + identifier);
+        //initWidget(link);
+        initWidget(title);
     }
 
     @Override
